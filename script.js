8f9c50ed8cbfd704b817ca756c3b3e42f1417725
ajasa shamsuedeen <shamsudeenajasa@gmail.com>
2023-11-15 13:39:04 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener('keydown', function (e){
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+        if(!audio) return; //stops the function from runnigg altogether
+        audio.currentTime= 0; //rewind o the start
+        audio.play();
+        key.classList.add('playing')
+},[])
+
+function removeTransition (e){
+    if (e.propertyName !== 'transform' ) return;
+    this.classList.remove('playing')
+}
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
